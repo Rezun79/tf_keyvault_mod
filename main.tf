d0cf54e1c5fee0bb20b76cd05d29341465fa6c51
rezun79 <rezun79@gmail.com>
2022-05-16 21:50:26 +0200

    Add secret

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_key_vault_secret" "top-secret" {
+  name         = "top-secret"
+  value        = data.azurerm_key_vault_secret.secret
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
