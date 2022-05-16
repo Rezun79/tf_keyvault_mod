7aa6783274636153185210ae3071f48d4019e970
rezun79 <rezun79@gmail.com>
2022-05-16 21:55:04 +0200

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault_secret" "top-secret" {
   name         = "top-secret"
-  value        = "${data.azurerm_key_vault_secret.secret}"
+  value        = data.azurerm_key_vault_secret.secret.value
   key_vault_id = data.azurerm_key_vault.keyvault.id
 }
